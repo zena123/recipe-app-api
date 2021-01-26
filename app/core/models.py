012f506e2168ac0adb2452a6745707b98a82aa3b
zainab96 <zinab.jom3a@gmail.com>
2021-01-26 13:33:28 +0200

    num 5

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """ creates and saves new user """
+        if not email:
+            raise ValueError('users must have valid email address')
+        # normalize_email: helper function comes with BaseUserManager
+        user= self.model(email=self.normalize_email(email), **extra_fields)
+        # need that helper function because our password is encrypted
+        user.set_password(password)
+        user.save(using=self.db) # for supporting multiple databases
+
+        return user
+
+    # no need to worry about extra_fields because we will create it with terminal
+    def create_superuser(self, email, password):
+        """ creates ans saves new superuser """
+        user= self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser= True
+        user.save(using=self._db)   # user must be saved because it was modified
+
+        return user
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """custom user model that supports using email instead of username"""
+    email= models.EmailField(max_length=255, unique=True)
+    name= models.CharField(max_length=255)
+    is_active= models.BooleanField(default=True)   # userin system is active or not. 
+    is_staff= models.BooleanField(default=False)   # staff user need special command to be created
+
+    objects=UserManager()
+
+    # username by default is : username but we customised it
+    USERNAME_FIELD='email'
+
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,47 @@
 #  importing user model directly from models not so good
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+#  to test if elper from model can create a new user
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """test creating new user with an email is successful"""
+        email= 'test@londonappdev.com'
+        password= 'Testpass123'
+        user= get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))   # assert true because of password that is hashed
+
+
+    def test_new_user_email_normalized(self):
+        """ if the email of a new user is normalized all lowercase"""
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    
+    # no empty or None value
+    def test_new_user_invalid_email(self):
+        """ test creates email with no email raises errors """
+        with self.assertRaises(ValueError):     # must raise valueError to pass test
+            user= get_user_model().objects.create_user(None, 'test123')
+    
+
+    def test_create_new_superuser(self):
+        """
+        to check that we created superuser and assigned is staff and is-supperuser
+        is_superuser is included as part of PermissionsMixin
+        """
+        user= get_user_model().object.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
