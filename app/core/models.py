de00528ceed8ca7a4db9f07065d05d31ecf69278
zainab <zeinab@kuwaitnet.dev>
2022-10-15 17:14:30 +0300

    feat: add custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -6,9 +6,20 @@ class UserManager(BaseUserManager):
     """ creates and saves a new user"""
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("email must be provided")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)  # this one for supporting multiple Databases
+        return user
+
+    def create_superuser(self, email, password):
+        """ creates and saves a new superuser
+         no need to worry about extra_fields cause we'll use this in command line"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,3 +9,20 @@ class TestUser(TestCase):
         user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user)
+
+    def test_new_user_email_normalized(self):
+        """ test email of new user created is normalized """
+        email = "test@GMAIL.COM"
+        user = get_user_model().objects.create_user(email=email)
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ test invalid user email"""
+        with self.assertRaises(ValueError):
+            """anything we run here should raise value error , else test will fail"""
+            get_user_model().objects.create_user(None, password="test123")
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser("test@gmail.com", "test123")
+        self.assertTrue(user.is_superuser)  # this comes with permissionsMixin
+        self.assertTrue(user.is_staff)
